0034bddc5de4ac76cb93da79415d61d5e33dcadd
LyDjons <znahar917@gmail.com>
2014-11-07 12:07:25 +0200

    test first class

---
+++ src/com/ClassLoader.java
@@ -0,0 +1,18 @@
+package com;
+
+
+import java.io.IOException;
+
+/**
+ * Created by LyDjons on 08.10.14.
+ */
+public class ClassLoader {
+
+    public static void main(String args[]) throws IOException {
+
+
+
+
+
+    }
+}
