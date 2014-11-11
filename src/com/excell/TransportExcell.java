18bdf7c05bccc79937276284db8db3e0000c19f3
LyDjons <znahar917@gmail.com>
2014-11-11 16:02:26 +0200

    update transportExcell

--- src/com/excell/TransportExcell.java
+++ src/com/excell/TransportExcell.java
@@ -13,7 +13,7 @@ import java.util.Set;
  */
 public class TransportExcell {
 
-    private Set<String> department;
+    private String department;
     private String transport_mark;
    public String gos;
     private String type_of_work;
@@ -24,28 +24,31 @@ public class TransportExcell {
    private ArrayList<Interval> intervals;
 
     public TransportExcell(Report report) {
-       //department
+       department= get_list_departments_of_work();
        transport_mark =report.getTransport();
        gos = report.getInfo().getGos();
        type_of_work = get_type_of_work();
-
-
-      // type_of_work = get_type_of_work();
-     //  fio  = report.getInfo().getDriver();
+       fio  = report.getInfo().getDriver();
 
     }
     private static String get_type_of_work(){
-        return "Збирання кукурудзи";
+        return "Збирання кукурудзи_Тест";
+    }
+    private static String get_list_departments_of_work(){
+        return "Хмільниця_Тест";
     }
 
     @Override
     public String toString() {
         return "TransportExcell{" +
-                "department=" + department +
+                "department='" + department + '\'' +
                 ", transport_mark='" + transport_mark + '\'' +
                 ", gos='" + gos + '\'' +
                 ", type_of_work='" + type_of_work + '\'' +
                 ", fio='" + fio + '\'' +
+                ", start=" + start +
+                ", end=" + end +
+                ", intervals=" + intervals +
                 '}';
     }
 
